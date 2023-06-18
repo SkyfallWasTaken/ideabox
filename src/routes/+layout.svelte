80f957422900815139509d14de3572203cfa741d
Mahad Kalam <mahadkalam1234@gmail.com>
2023-06-18 14:51:34 +0100

    Remove Supabase Auth - prepare to switch to Firebase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,27 +6,6 @@
 
 	import MainNav from '$components/main-nav/MainNav.svelte';
 	import WelcomeDialog from '$components/welcome-dialog/WelcomeDialog.svelte';
-
-	import { invalidate } from '$app/navigation';
-	import { onMount } from 'svelte';
-	import type { SupabaseClient } from '@supabase/supabase-js';
-
-	export let data;
-
-	let { supabase, session } = data;
-	$: ({ supabase, session } = data);
-
-	onMount(() => {
-		const {
-			data: { subscription }
-		} = supabase.auth.onAuthStateChange((event, _session) => {
-			if (_session?.expires_at !== session?.expires_at) {
-				invalidate('supabase:auth');
-			}
-		});
-
-		return () => subscription.unsubscribe();
-	});
 </script>
 
 <MainNav />
